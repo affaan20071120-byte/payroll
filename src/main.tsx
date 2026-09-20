669147ebe18460dd780424960501247a13045591
Affaan <affaan20071120@gmail.com>
2026-09-20 21:16:19 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = 'PASTE_YOUR_CLIENT_ID_HERE'
+const GOOGLE_CLIENT_ID = '818839210214-3304b0sar8432u1ag9mvo2l6c0s8d2q2.apps.googleusercontent.com'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
